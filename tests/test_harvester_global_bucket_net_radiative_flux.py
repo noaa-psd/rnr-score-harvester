b3ccc0e5c9ef4853b37e01493c5c26714b11d16f
Sherrie Fredrick <Sherrie.Fredrick@noaa.gov>
2023-09-13 12:58:10 -0600

    Initial commit of test_global_bucket_net_radiative_flux.py

--- tests/test_harvester_global_bucket_net_radiative_flux.py
+++ tests/test_harvester_global_bucket_net_radiative_flux.py
@@ -29,37 +29,52 @@ VALID_CONFIG_DICT = {
     'harvester_name': hv_registry.GLOBAL_BUCKET_NET_RADIATIVE_FLUX,
     'filenames' : BFG_PATH,
     'statistic': ['mean'],
-    'variables': ['dswrf_avetoa','ulwrf_avetoa','uswrf_avetoa']
+    'variables': ['dswrf_avetoa','ulwrf_avetoa','uswrf_avetoa','net_radiative_flux'],
+    'cycletime': ['median_time']
  }
 
-def test_NetRadiativeFlux_harvester_get_files():
-    data1 = harvest(VALID_CONFIG_DICT)  
-    assert type(data1) is list
-    assert len(data1) > 0
-    assert data1[0].filenames==BFG_PATH
-
-def test_NetRadiativeFlux_harvester_variables():
+def test_NetRadiativeFlux_harvester_names(data1):
     """ These variables are from the Netcdf bfg files above.
     dswrf_avetoa - top of atmos downward shortwave flux
     ulwrf_avetoa - top of atmos upward longwave flux
     uswrf_avetoa - top of atmos upward shortwave flux 
     """
-    data1 = harvest(VALID_CONFIG_DICT)
     assert data1[0].variables == 'dswrf_avetoa'
     assert data1[1].variables == 'ulwrf_avetoa'
     assert data1[2].variables == 'uswrf_avetoa'
 
-def test_NetRadiativeFlux_harvester_units():
-    """The units of all the requested variables are 
+def test_NetRadiativeFlux_harvester_units(data1):
+    """The units of all the requested variables are
        the same W/m**2"""
-    data1 = harvest(VALID_CONFIG_DICT)
     assert data1[0].units == 'W/m**2'
 
+def test_NetRadiativeFlux_harvester_values(data1):
+    """Here we test to see if all of the requested variable means
+       have valid values. The hard coded values of mean_dswrf, 
+       mean_ulwrf, mean_uswrf and mean_net_radiative_flux
+       were calculated in a separate python code using the 
+       above netcdf files.  The top of the atmosphere net 
+       radiative flux is defined as mean_dswrf - mean_ulwrf - mean_uswrf"""
+    mean_dswrf = np.float32(326.76144)
+    assert data1[0].value == mean_dswrf 
+    mean_ulwrf = np.float32(228.17543)
+    assert data1[1].value == mean_ulwrf
+    mean_uswrf = np.float32(109.79811)
+    assert data1[2].value == mean_uswrf
+    mean_net_radiative_flux = np.float32(-11.212097)
+    assert data1[3].value == mean_net_radiative_flux
+
+def test_NetRadiativeFlux_harvester():
+    data1 = harvest(VALID_CONFIG_DICT)  
+    assert type(data1) is list
+    assert len(data1) > 0
+    assert data1[0].filenames==BFG_PATH
+    test_NetRadiativeFlux_harvester_names(data1) 
+    test_NetRadiativeFlux_harvester_units(data1)
+    test_NetRadiativeFlux_harvester_values(data1)
 
 def main():
-    test_NetRadiativeFlux_harvester_get_files()
-    test_NetRadiativeFlux_harvester_variables()
-    test_NetRadiativeFlux_harvester_units()
+    test_NetRadiativeFlux_harvester()
 
 if __name__=='__main__':
     main()
