bd3a50fe3bb293d40f1468eca650d7f3f44846ed
Sherrie Fredrick <Sherrie.Fredrick@noaa.gov>
2024-04-18 08:56:00 -0600

    Tests to make sure that the region values passed in my the calling script are within bounds was added to the add_user_region function in the region_utils script.

--- src/score_hv/region_utils.py
+++ src/score_hv/region_utils.py
@@ -51,9 +51,38 @@ class GeoRegions:
           - new_east_lon: Eastern longitude of the new region.
           - new_west_lon: Western longitude of the new region.
         """
-
         if not isinstance(new_name, str):
            msg = f'name must be a string - name {new_name}'
+           raise ValueError(msg)
+        
+        if new_min_lat > new_max_lat:  
+           msg = f'minimum latitude must be less than maximum latitude ' \
+                 f'min_lat: {new_min_lat}, max_lat: {new_max_lat}'  
+           raise ValueError(msg)
+         
+        if new_min_lat < -90. or new_min_lat > 90.:
+           msg = f'minimum latitude must be greater than -90. and less than 90. '\
+                 f'min_lat: {new_min_lat}'
+           raise ValueError(msg)
+
+        if new_max_lat > 90.0  or new_max_lat < -90.:
+           msg = f'maximum latitude must be greater than -90. and less than 90. '\
+                 f'max_lat: {new_max_lat}'
+           raise ValueError(msg)        
+         
+        if new_east_lon > new_west_lon:
+            msg = f'west longitude must be greater then east_longitude ' \
+                  f'west_lon: {new_west_lon}, east_lon: {new_east_lon}'
+            raise ValueError(msg)
+
+        if new_east_lon < 0.0 or new_east_lon > 360.:
+           msg = f'east longitude must be greater than 0.0 and less than 360. ' \
+                 f'east_lon: {new_east_lon}'
+           raise ValueError(msg)
+        
+        if new_west_lon > 360.0 or  new_west_lon < 0.0:
+           msg = f'west longitude must be less than 360.0 and greater than 0.0 ' \
+                 f'west_lon: {new_west_lon}'
            raise ValueError(msg)
 
         self.name.append(new_name)
@@ -72,6 +101,7 @@ class GeoRegions:
                 'east_lon': self.east_lon[i],
                 'west_lon': self.west_lon[i],
             }
-        return user_regions 
+            user_regions.append(region_info)
+        return user_regions
 
         
