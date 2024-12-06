241548f4ffbc39d1007dda080c7bc6bf9802f69f
Adam Schneider <adam.schneider@noaa.gov>
2024-12-05 17:45:45 -0700

    resumed development of the harvester for GSI analysis fit to conventional observation statistics

--- src/score_hv/harvesters/gsi_conventional_obs.py
+++ src/score_hv/harvesters/gsi_conventional_obs.py
@@ -1,5 +1,5 @@
 """methods to extract information from the Gridpoint Statistical Interpolation (GSI) analysis output (fit files), including innovation statistics for
-conventional observations (including satellite retrievals)
+conventional observations
 """
 
 import os
@@ -29,9 +29,10 @@ VALID_STATISTICS = (
     'qcpen' # nonlinear qc penalty
 )
 
-ObservationData = namedtuple(
+HarvestedData = namedtuple(
     'HarvestedData',
     ['datetime', # datetime.datetime object (date and a time)
+     'ensemble_member'
      'plevs_top', # pressures at the tops of the layers (for multi-level data)
      'plevs_bot', # pressure at the bottoms of the layers (for multi-level data)
      'variable',
@@ -39,14 +40,14 @@ ObservationData = namedtuple(
      'values',
      'units',
      'longname',
-     'iteration' # GSI outer loop number
+     'iteration', # GSI outer loop number
      'usage' # used (asm), read in but not assimilated (mon) or rejected (rej)
      'type', # prepbufr obs type    
     ]
 )
 
 @dataclass
-class ConvObs(ConfigInterface):
+class ConvObsConfig(ConfigInterface):
     
     config_data: dict = field(default_factory=dict)
     
@@ -54,10 +55,82 @@ class ConvObs(ConfigInterface):
         self.set_config()
         
     def set_config(self):
-        """function to set configuration variables given dictionary
+        """ function to set configuration variables from given dictionary
         """
-        pass
+        self.harvest_filename = self.config_data.get('filename')
+        self.set_variables()
+        self.set_statistics()
+    
+    def set_variables(self):
+        self.vars_to_harvest = self.config_data.get('variables')
+        if self.vars_to_harvest == None:
+            self.vars_to_harvest = list()
         
+        for var in self.vars_to_harvest:
+            if var not in VALID_VARIABLES:
+                msg = (f"{var} is not a supported variable "
+                       "to harvest from the GSI analysis fort.201, fort.205 "
+                       "and fort.213 fit files. "
+                       "Please reconfigure the input dictionary using only the "
+                       f"following variables: {VALID_VARIABLES}")
+                raise KeyError(msg)
+    
+    def set_statistics(self):
+        self.stats_to_harvest = self.config_data.get('statistics')
+        if self.stats_to_harvest == None:
+            self.stats_to_harvest = list()
+            
+        for stat in self.stats_to_harvest:
+            if stat not in VALID_STATISTICS:
+                msg = (f"{stat} is not a supported statistic "
+                       "to harvest from the GSI analysis fort.201, fort.205 "
+                       "and fort.213 fit files. "
+                       "Please reconfigure the input dictionary using only the "
+                       f"following statistics: {VALID_STATISTICS}")
+                raise KeyError(msg)
+                
+@dataclass
+class ConvObsHv(object):
+    
+    config: ConvObsConfig = field(default_factory = ConvObsConfig)
+    
+    def get_data(self):
+        """Read the fit file (from the GSI analysis output)"
+        
+        returns a list of HarvestedData tuples
+        """
+        self.plevs_top = dict()
+        self.plevs_bot = dict()
+        
+        # get the datetime from the input file name
+        try: # format is gsistats.YYYYMMDDHH_control
+            self.datetime = datetime.strptime(
+                self.config.harvest_filename.split('.')[-1].split('_')[0],
+                '%Y%m%d%H')
+            self.ensemble_member = self.config.harvest_filename.split('.')[-1].split('_')[1]
+            
+        except ValueError as err:
+            if self.config.harvest_filename[-5:] == 'z.txt':
+                # assume format from NASA
+                self.datetime = datetime.strptime(
+                    self.config.harvest_filename.split('.')[-2],
+                    '%Y%m%d_%Hz'
+                )
+                self.ensemble_member = 'control'
+            else:
+                raise ValueError(f'{self.config.harvest_filename} is not a '
+                 'supported GSI fit file name: cannot return datetime') from err
+        
+        with open(self.config.harvest_filename, encoding="utf-8") as f:
+            self.lines = list(f)
+            
+        self.parse_fit_file()
+        
+    def parse_fit_file(self):
+        """ parse lines of fit file and extract statistics
+        """
+                
+                
 def test(gsistats_test_file):
     test_datetime = datetime.strptime(
         gsistats_test_file.split('.')[-1].split('_')[0], '%Y%m%d%H')
