e9e96a450df6ad32835fb0783bc65d4b10ae2f8b
sherrieF <45428753+sherrieF@users.noreply.github.com>
2025-01-16 14:59:14 -0700

    Update get_unit_test_data.sh

--- get_unit_test_data.sh
+++ get_unit_test_data.sh
@@ -1,6 +1,16 @@
 #!/bin/bash --posix
 
-# grab select aws files stored in the bdp
-aws s3 cp s3://noaa-reanalyses-pds/ufsrnr.baselines/ufsrnr.v1.0.linux_hera/C96L64.UFSRNR.GSI_SOCA_3DVAR.012016/gsi/innov_stats.uvwind.2016010300.nc data/innov_stats.uvwind.2016010300.nc --no-sign-request
-aws s3 cp s3://noaa-reanalyses-pds/ufsrnr.baselines/ufsrnr.v1.0.linux_hera/C96L64.UFSRNR.GSI_SOCA_3DVAR.012016/gsi/innov_stats.uvwind.2016010300.nc data/innov_stats.temperature.2016010300.nc --no-sign-request
-aws s3 cp s3://noaa-reanalyses-pds/ufsrnr.baselines/ufsrnr.v1.0.linux_hera/C96L64.UFSRNR.GSI_SOCA_3DVAR.012016/gsi/innov_stats.uvwind.2016010300.nc data/innov_stats.spechumid.2016010300.nc --no-sign-request
+#The test data files for running the tests for the output of the daily_bfg.py in in the following AWS bucket.
+#These files are Netcdf format files.  The files that start with bfg_date_fhr(hr)_fieldname_control.nc have the following
+#data fields in them.
+# grid_xt - longitude
+# grid_yt - latitude
+# time calendar = JULIAN
+# field values - model output
+# lfrac - land fraction 
+#sotyp - soil type in integer 1-9
+#To get a listing of the Netcdf files in the bucket type:
+aws s3 ls s3://noaa-reanalyses-pds/score_suite/test_data/
+#To copy a file to a local directory. cd to local directory then type:
+aws s3 cp s3://noaa-reanalyses-pds/score_suite/test_data/filenname . --no-sign-request
+xxaws s3 cp s3://noaa-reanalyses-pds/ufsrnr.baselines/ufsrnr.v1.0.linux_hera/C96L64.UFSRNR.GSI_SOCA_3DVAR.012016/gsi/innov_stats.uvwind.2016010300.nc data/innov_stats.spechumid.2016010300.nc --no-sign-request
